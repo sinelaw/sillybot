de6cefb0fdf435ebb393f82d1d78c11ddac5c94f
Noam Lewis <noam.lewis@elastifile.com>
2018-09-22 21:58:51 +0300

    WIP

--- robu.py
+++ robu.py
@@ -17,28 +17,25 @@ def func():
     microphone = detect_noise.get_microphone()
     setup()
     time.sleep(5)
+    for pin in pins:
+        GPIO.output(pin, 0)
+    cycle = 0.2
+    speed = 1
+    directions = [(0,0), (0,1), (1,0), (1,1)]
+    direction = directions[0]
     while True:
         sound = detect_noise.get_sound(microphone)
-        for pin in pins:
+        if not detect_noise.is_silence(sound):
+            direction = random.choice(directions)
+            speed = random.random() * 0.5 + 0.5
+
+        for pin, val in zip(pins, direction):
+            GPIO.output(pin, val)
+        time.sleep(cycle * speed)
+
+        for pin, val in zip(pins, direction):
             GPIO.output(pin, 0)
-        if random.random() > 0.02:
-            continue
-        cycle = 0.2
-        directions = [(0,0), (0,1), (1,0), (1,1)]
-        direction = directions[0]
-        while True:
-            sound = detect_noise.get_sound(microphone)
-            if not detect_noise.is_silence(sound):
-                direction = random.choice(directions)
-                speed = random.random() * 0.5 + 0.5
-
-            for pin, val in zip(pins, direction):
-                GPIO.output(pin, val)
-            time.sleep(cycle * speed)
-
-            for pin, val in zip(pins, direction):
-                GPIO.output(pin, 0)
-            time.sleep(cycle * (1 - speed))
+        time.sleep(cycle * (1 - speed))
 
 if __name__ == '__main__':
     func()
