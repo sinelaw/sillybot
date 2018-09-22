0c13038b48f8d33b4019ec73eb2fb63947c989d8
Noam Lewis <jones.noamle@gmail.com>
2018-09-22 18:48:30 +0000

    WIP

--- play.py
+++ play.py
@@ -2,22 +2,24 @@
 
 import alsaaudio, time, audioop
 
-
-# Open the device in playback mode.
-speaker = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
-speaker.setchannels(1)
-speaker.setrate(8000)
-speaker.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-speaker.setperiodsize(160)
+def open_speaker():
+    # Open the device in playback mode.
+    speaker = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
+    speaker.setchannels(1)
+    speaker.setrate(44000)
+    speaker.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+    speaker.setperiodsize(160)
+    return speaker
 
 def is_silence(sound):
     return audioop.max(sound, 2) < 900
 
 def record_stuff(filename):
+    print "initing..."
+    speaker = open_speaker()
+    print "inited"
     with open(filename, 'r') as f:
         speaker.write(f.read())
-
-
 
 import sys
 if __name__ == '__main__':
--- robu.py
+++ robu.py
@@ -19,30 +19,33 @@ def func():
     time.sleep(5)
     while True:
         sound = detect_noise.get_sound(microphone)
-        if detect_noise.is_silence(sound):
+        stuck = not detect_noise.is_silence(sound)
         #    time.sleep(0.1)
-            for pin in pins:
-                GPIO.output(pin, 0)
-            if random.random() > 0.02:
-                continue
-        while random.random() < 0.9:
+        for pin in pins:
+            GPIO.output(pin, 0)
+        if random.random() > 0.02:
+            continue
+        direction = (1,1)
+        while True: #xrange(1): #False: #random.random() < 0.9:
             directions = [(0,0),(0,1),(1,0),(1,1)]
-            direction = random.choice(directions)
+            sound = detect_noise.get_sound(microphone)
+
+            if not detect_noise.is_silence(sound):
+                direction = random.choice(directions)
             start = time.time()
             cycle = 0.2
             target_freq = random.random()
-            freq = 0.1
-            while freq < target_freq and freq < 1.0:
+            freq = 1#0.1
+            for i in xrange(2): #while freq < target_freq and freq < 1.0:
                 #print freq
-                for i in xrange(2):
+                for i in xrange(1):#for i in xrange(2):
                     for pin, val in zip(pins, direction):
                         GPIO.output(pin, val)
-                    time.sleep(cycle * freq)
-                    for pin, val in zip(pins, direction):
-                        GPIO.output(pin, 0)
-                    time.sleep(cycle * (1 - freq))
-
-                freq += target_freq / 2
+                    #for pin, val in zip(pins, direction):
+                    #    GPIO.output(pin, 0)
+                    #time.sleep(cycle * (1 - freq))
+                time.sleep(cycle)
+                #freq += target_freq / 2
 
 if __name__ == '__main__':
     func()
